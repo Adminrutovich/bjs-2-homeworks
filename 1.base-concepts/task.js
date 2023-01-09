52994a4f0826263a1d34c6a5d49e4bc441c63220
Admin Rutovich <380662620485@ukr.net>
2023-01-09 18:21:13 +0200

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,42 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b**2-4*a*c;
+  if(d < 0) {
+    return arr;
+  }
+  else if (d > 0) {
+    arr[0] = (-b + Math.sqrt(d))/(2*a);
+    arr[1] = (-b - Math.sqrt(d))/(2*a);
+  } 
+  else if (d === 0) {
+    arr[0] = -b/(2*a);
+  }
   return arr;
 }
+solveEquation(2, 6, 12);
+solveEquation(9, 100, 1);
+solveEquation(54, 13, -2);
+
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  if (isNaN(percent) || isNaN(contribution) || isNaN(amount)) {
+    return false;
+  }
+  if(countMonths < 0 || amount < 0) {
+    return false;
+  } 
+  let P = percent/100/12;
+  let S = amount - contribution;
+  let monthlyPayment = S * (P + (P / (((1 + P)**countMonths) - 1)));
+  let totalPayment = monthlyPayment * countMonths;
+  return Number(totalPayment.toFixed(2));
 }
+calculateTotalMortgage(10, 0, 50000, 12);
+calculateTotalMortgage(10, 1000, 50000, 12);
+calculateTotalMortgage(10, 0, 20000, 24);
+calculateTotalMortgage(10, 1000, 20000, 24);
+calculateTotalMortgage(10, 20000, 20000, 24);
+calculateTotalMortgage(10, 0, 10000, 36);
+calculateTotalMortgage(15, 0, 10000, 36);
