7b2e4068eca32b002ca43d232c6c131a23e31d55
Admin Rutovich <380662620485@ukr.net>
2023-02-06 17:53:40 +0200

    Шестое задание почти готово (есть ошибки)

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,50 @@
-﻿
+﻿function parseCount(value) {
+   if (Number.parseFloat(value) === NaN) {
+     throw new Error("Невалидное значение");
+    }
+    return Number.parseFloat(value);
+}
+function validateCount(value) {
+    try {
+        return parseCount(value);
+    } 
+        catch (error) {
+        console.log(error);
+        }
+
+}
+
+class Triangle {
+
+    constructor(sideA, sideB, sideC) { 
+        
+        if (sideA + sideB < sideC || sideC + sideB < sideA || sideC + sideA < sideB) {
+            throw new Error('Треугольник с такими сторонами не существует'); 
+        }
+        this.sideA = sideA;
+        this.sideB = sideB;
+        this.sideC = sideC;
+    }
+        get perimeter() {
+            return (this.sideA + this.sideB + this.sideC);
+        }
+        get area() {
+            return  Number(0.25 * Math.sqrt((this.sideA + this.sideB - this.sideC) * (this.sideA - this.sideB + this.sideC) * (this.sideC + this.sideB - this.sideA ) * (this.sideA + this.sideB + this.sideC))); 
+        } 
+}
+
+function getTriangle(sideA, sideB, sideC) {
+    try {
+        return new Triangle(sideA, sideB, sideC)
+    }
+    catch(error) {
+        return {
+            get perimeter() {
+                return 'Ошибка! Треугольник не существует';
+            },
+            get area() {
+                return  'Ошибка! Треугольник не существует'; 
+            } 
+        } 
+    }
+}
